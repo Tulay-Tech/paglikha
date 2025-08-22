9233ca4e73498c516fdfe3fd44777ce659abe66a
Alysstair Morales <al@tulaytech.co>
2025-08-23 00:35:04 +0800

    added auth setup for frontend

--- templates/packages/web/src/routes/index.tsx
+++ templates/packages/web/src/routes/index.tsx
@@ -1,39 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
-
-export const Route = createFileRoute('/')({
-  component: App,
-})
+import { useAuth } from "@/components/AuthContext";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+  const auth = useAuth();
+  const [status, setStatus] = useState("");
+
+  async function callApi() {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}me`, {
+      headers: {
+        Authorization: `Bearer ${await auth.getToken()}`,
+      },
+    });
+
+    setStatus(res.ok ? "success" : "error");
+  }
+
+  return !auth.loaded ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      {auth.loggedIn ? (
+        <div>
+          <p>
+            <span>Logged in</span>
+            {auth.userId && <span> as {auth.userId}</span>}
+          </p>
+          {status !== "" && <p>API call: {status}</p>}
+          <button onClick={callApi}>Call API</button>
+          <button onClick={auth.logout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={auth.login}>Login with OAuth</button>
+      )}
     </div>
-  )
+  );
 }
+
+export default App;
